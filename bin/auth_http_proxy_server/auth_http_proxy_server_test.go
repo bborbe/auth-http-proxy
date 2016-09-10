4117f1b1ba3427480f7128d3bd6beb3e04696288
Benjamin Borbe <bborbe@rocketnews.de>
2016-09-10 18:53:38 +0200

    add test create config

--- bin/auth_http_proxy_server/auth_http_proxy_server_test.go
+++ bin/auth_http_proxy_server/auth_http_proxy_server_test.go
@@ -23,3 +23,14 @@ func TestGetVerifierByType(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestCreateConfig(t *testing.T) {
+	config, err := createConfig()
+	if err := AssertThat(err, NilValue()); err != nil {
+		t.Fatal(err)
+	}
+	if err := AssertThat(config, NotNilValue()); err != nil {
+		t.Fatal(err)
+	}
+
+}
